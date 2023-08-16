0d939fa5678540c918e2965665691ace3283c28e
q4artz <116222352+q4artz@users.noreply.github.com>
2023-08-16 09:49:55 +0800

    Add files via upload

--- untitled2/src/Main.java
+++ untitled2/src/Main.java
@@ -1,33 +1,62 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.Random;
 public class Main {
-    public static void main(String[] args) {
-        ticket SavannaTicket = new ticket("Savanna",20.00);
-        ticket RiverTicket = new ticket("River",20.00);
-        ticket ArticTicket = new ticket("Artic",50.00);
-        ticket AllAccessTicket = new ticket("All",80.00);
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        ticket SavannaTicket = new ticket("Savanna", 20.00);
+        ticket RiverTicket = new ticket("River", 20.00);
+        ticket ArticTicket = new ticket("Artic", 50.00);
+        ticket AllAccessTicket = new ticket("All", 80.00);
 
         // 0 == quit 1 == Buy 2 == Search
         int option = 0;
-        System.out.println("Welcome Customer, What would you like to do");
-        System.out.println("");
-    }
+        System.out.println("\nWelcome Customer, What would you like to do");
+        System.out.println("Please input \n \"1\" to Buy \n \"2\" to Search \n \"0\" to Quit \n");
+        option = scanner.nextInt();
+        scanner.nextLine();
+        System.out.printf("You Entered %d\n", option);
 
-    void LoginUser(){
+        do{
+            if (option == 1) {
+                System.out.println("[+] Transferring to Purchasing Option");
+                Purchasing();
+            } else if (option == 2) {
+                System.out.println("[+] Transferring to Searching Option");
+                LoginUser();
+            }
+        }while (option != 0);
+        System.out.println("[-] Exiting...");
+        System.exit(0);
+    }
+    static void Purchasing(){
+        Scanner scanner = new Scanner(System.in);
+        String option;
+        System.out.println("[+] Which ticket would you like to buy? \"All Access Ticket (RM 80) Enter \"All\" \"\n \"Artic Ticket (RM50) Enter\"Artic\" \"\n \"Savanna Ticket (RM20)\" Enter \"Savanna\" \n \"River Ticket (RM20)\" Enter \"River\"\n");
+        option = scanner.nextLine();
+
+        if(option.equalsIgnoreCase("All")){
+
+        }
+
+    }
+    static void LoginUser() throws IOException {
         Scanner scanner = new Scanner(System.in);
 
         String username;
         String password;
-        String cardnumber;
-        int ccv;
-        int expdate;
 
         System.out.println("[+] Please Enter Your Name");
         username = scanner.nextLine();
         System.out.println("[+] PLease Enter Your password");
         password = scanner.nextLine();
+
+        ReadFromFile(username,password);
     }
-    void RegisterUser(){
+    void RegisterUser() throws IOException {
 
         Scanner scanner = new Scanner(System.in);
         Random RandomNumber = new Random();
@@ -51,6 +80,7 @@ public class Main {
         scanner.nextLine();
         System.out.println("[+] Enter expdate");
         expdate = scanner.nextInt();
+        scanner.nextLine();
 
         // Randomly generate an amount of money
         double money = RandomNumber.nextDouble(1234.99)+1;
@@ -62,13 +92,25 @@ public class Main {
         System.out.println("[+] Your account Has been created");
 
     }
-    void WriteToFile(user Customer){
-        // File Function
+    void WriteToFile(user Customer) throws IOException{
+        // Create The file
+        File file = new File("user.txt");
+
+        // Creates a File writer class
+        FileWriter fw = new FileWriter(file);
+
+        // Creates a print writer class
+        PrintWriter pw = new PrintWriter(fw);
+
+        pw.println("line 1");
+        pw.println("line 2");
+        pw.println("line 3");
+
+        pw.close();
     }
-    void ReadFromFile(String username,String password){
+    static void ReadFromFile(String username, String password)throws IOException{
 
     }
 
 
 }
-
